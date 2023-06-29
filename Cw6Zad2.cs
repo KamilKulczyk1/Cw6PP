f45675c2be051a71688140adf4a1734e357b935d
KamilKulczyk1 <128227168+KamilKulczyk1@users.noreply.github.com>
2023-06-29 13:12:35 +0200

    Initial commit

---
+++ Cw6Zad2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj rozmiar tablicy: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] tab = new int[n, n];
+        int k = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                tab[i, j] = k;
+                k++;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(tab[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
